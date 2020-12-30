a8cf8a9c9fe9648ae226df6dd8656f4c0434db64
glu <vadimgirgenson@yandex.ru>
2020-12-30 13:22:02 +0300

    Models of Categories and products added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [BASE_DIR / 'ecommerce/static/']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'ecommerce/static/media'
+
