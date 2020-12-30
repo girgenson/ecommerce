05c23fee9a5a5adce06fe5c9332a8929436c4cdb
glu <vadimgirgenson@yandex.ru>
2020-12-30 08:01:47 +0300

    About page turned to specific product page

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,5 @@ from shop import views
 
 urlpatterns = [
     path('', views.home, name="go_to_home_page"),
-    path('about_app', views.about, name="go_to_about_page")
+    path('product', views.product, name="go_to_product_page")
 ]
