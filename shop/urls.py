55c15edfd1ee720f98155d179f99562660c9cbd4
glu <vadimgirgenson@yandex.ru>
2020-12-31 12:22:06 +0300

    The main page is almost finished

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from shop import views
 
 urlpatterns = [
     path('', views.home, name="go_to_home_page"),
-    path('product', views.product, name="go_to_product_page")
+    path('product', views.product, name="go_to_product_page"),
+    path('<slug:category_slug>', views.home,
+         name='products_by_category'),
 ]
